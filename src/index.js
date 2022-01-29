dfc9f8f1aa4f0b15be86ec73aae1dfd8b6446bdc
CWKSC <53114952+CWKSC@users.noreply.github.com>
2022-01-30 04:47:31 +0800

    Update Card, onMouseMove

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import { getData } from "./utils"
 
 import { TypeChooser } from "react-stockcharts/lib/helper";
 
+import {Card, CardContent, Stack, TextField, Typography } from "@mui/material";
+import Box from '@material-ui/core/Box'
+import { Label } from 'react-bootstrap';
+
 class ChartComponent extends React.Component {
 	componentDidMount() {
 		getData().then(data => {
@@ -26,6 +30,23 @@ class ChartComponent extends React.Component {
 }
 
 render(
-	<ChartComponent />,
+	<Box sx={{ padding: "3rem" }}>
+		<Card elevation="4" sx={{ margin: "1rem", padding: "1rem", borderRadius: "1rem" }}  >
+			<CardContent onMouseMove={(event) => {
+					var dom = document.querySelector('#root > div > div > div > div.react-stockchart > svg > g > g > g:nth-child(1) > g:nth-child(10) > g > text > tspan:nth-child(2)')
+					var date = dom ? dom.textContent : "n/a"
+					console.log(date)
+				}}>
+				<Stack sx={{ mx: "2rem" }} flexDirection="row"  alignItems="center" columnGap=".5rem"  >
+					<Typography>
+						Symbol:
+					</Typography>
+					<TextField variant="standard" />
+				</Stack>
+				<ChartComponent />
+			</CardContent>
+		</Card>
+	</Box>
+	,
 	document.getElementById("root")
 );
